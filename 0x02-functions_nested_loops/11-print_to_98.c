41a272ef588741f34f63eda3985c375e27479c12
cleavon3 <cleavon.design@gmail.com>
2023-08-15 19:03:30 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,8 +9,6 @@
 void print_to_98(int n)
 {
 int i, j;
-
-
 if (n <= 98)
 {
 for (i = n; i <= 98; i++)
@@ -20,7 +18,8 @@ printf("%d, ", i);
 else if (i == 98)
 printf("%d\n", i);
 }
-} else if (n >= 98)
+} 
+else if (n >= 98)
 {
 for (j = n; j >= 98; j--)
 {
