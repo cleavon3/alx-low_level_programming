add2ddbf11b71c9a6250b4813ce9e847720393e3
cleavon3 <cleavon.design@gmail.com>
2023-08-30 16:51:14 -0700

    all updated

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,21 +1,22 @@
 #include "main.h"
+
 /**
-* _pow_recursion - Computes the result of x raised to the power of y.
-* @x: The base value.
-* @y: The exponent value.
-*
-* Return: The result of x raised to the power of y.
-*/
+ * _pow_recursion - Computes the result of x raised to the power of y.
+ * @x: The base value.
+ * @y: The exponent value.
+ *
+ * Return: The result of x raised to the power of y.
+ */
 int _pow_recursion(int x, int y)
 {
-int partial_power;
+	int partial_power;
 
-if (y < 0)
-return -1;
+	if (y < 0)
+		return (-1);
 
-if (y == 0)
-return 1;
+	if (y == 0)
+		return (1);
 
-partial_power = _pow_recursion(x, y - 1);
-return x * partial_power;
+	partial_power = _pow_recursion(x, y - 1);
+	return (x * partial_power);
 }
