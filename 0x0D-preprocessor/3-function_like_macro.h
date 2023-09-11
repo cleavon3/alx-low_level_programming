6cac5ff39f965e12086ae72086e5d5021764a5f5
cleavon3 <cleavon.design@gmail.com>
2023-09-11 16:25:23 -0700

    all task completed

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * ABS - Computes the absolute value of a number.
+ * @x: The number to calculate the absolute value of.
+ *
+ * Return: The absolute value of the number x.
+ */
+#define ABS(x) ((x) < (0) ? -(x) : (x))
+
+#endif /* FUNCTION_LIKE_MACRO_H */
