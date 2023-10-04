41fa59affdffbee94eaccdeda01b637887f6e7aa
cleavon3 <cleavon.design@gmail.com>
2023-10-04 13:55:58 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * create_buffer - Allocates a 1024-byte buffer.
  * @file: The name of the file for error messages.
@@ -16,19 +17,24 @@ exit(99);
 }
 return (buffer);
 }
+
 /**
  * close_file - Closes a file descriptor.
  * @fd: The file descriptor to be closed.
+ *
+ * Return: 1 on success, -1 on failure.
  */
-void close_file(int fd)
+int close_file(int fd)
 {
 int status = close(fd);
 if (status == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-exit(100);
+return (-1);
 }
+return (1);
 }
+
 /**
  * main - Copies the contents of one file to another.
  * @argc: The number of arguments supplied to the program.
@@ -49,14 +55,14 @@ buffer = create_buffer(argv[2]);
 from_fd = open(argv[1], O_RDONLY);
 if (from_fd == -1)
 {
-dprintf(STDERR_FILENO, "Error: can't open file %s for reading\n", argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't open source file %s for reading\n", argv[1]);
 free(buffer);
 return (98);
 }
 to_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 if (to_fd == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't open file %s for writing\n", argv[2]);
+dprintf(STDERR_FILENO, "Error: Can't open destination file %s for writing\n", argv[2]);
 free(buffer);
 return (99);
 }
@@ -67,6 +73,8 @@ if (bytes_written == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to destination file %s\n", argv[2]);
 free(buffer);
+close_file(from_fd);
+close_file(to_fd);
 return (99);
 }
 }
@@ -74,11 +82,15 @@ if (bytes_read == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from source file %s\n", argv[1]);
 free(buffer);
+close_file(from_fd);
+close_file(to_fd);
 return (98);
 }
 free(buffer);
-close_file(from_fd);
-close_file(to_fd);
+if (close_file(from_fd) == -1 || close_file(to_fd) == -1)
+{
+return (100);
+}
 return (0);
 }
 
